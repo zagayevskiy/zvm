52ce9a61e85f57d3463e36080772ad715060a422
Denis Zagayevskiy <zagayevskiy@gmail.com>
2019-10-13 22:40:22 +0300

    change parsing

---
+++ test/com/zagayevskiy/zvm/zc/vmoverzc/BytecodeLoadingOnZcTest.kt
@@ -0,0 +1,12 @@
+package com.zagayevskiy.zvm.zc.vmoverzc
+
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+
+@RunWith(Parameterized::class)
+internal class BytecodeLoadingOnZcTest {
+
+
+
+}
--- test/testdata/sources/zc/vm/BytecodeParserZc.kt
+++ test/testdata/sources/zc/vm/BytecodeParserZc.kt
@@ -8,22 +8,29 @@ internal fun includeBytecodeParser() = """
         var globalsCount: int;
     }
 
-    struct FunctionInfo {
+    struct RawFunctionInfo {
         var address: int;
         var argsCount: int;
-        var localsCount: int;
+        var ld0: int;
+        var ld1: int;
+    }
+
+    struct RuntimeFunction {
+        var address: int;
+        var argsMemorySize: int;
     }
 
     struct ProgramInfo {
-        var serviceInfo: ServiceInfo;
+        var mainIndex: int;
         var functionsTable: [FunctionInfo];
+        var functionsTableSize: int;
         var bytecode: [byte];
         var bytecodeSize: int;
     }
 
     fn parseBytecode(rawBytecode: [byte], rawBytecodeSize: int): ProgramInfo {
         val serviceInfoSize = sizeof<ServiceInfo>;
-        val functionInfoSize = sizeof<FunctionInfo>;
+        val functionInfoSize = sizeof<RawFunctionInfo>;
         var cursor: [void] = rawBytecode;
 
         val serviceInfo: ServiceInfo = cursor;
@@ -31,18 +38,44 @@ internal fun includeBytecodeParser() = """
 
         val bytecodeSize = rawBytecodeSize - (serviceInfoSize + functionInfoSize*serviceInfo.functionsCount);
 
-        val functionsTable: [FunctionInfo] = alloc(4*serviceInfo.functionsCount);
+        val runtimeFunctionSize = sizeof<RuntimeFunction>
+        val functionsTable: [RuntimeFunction] = alloc(4*serviceInfo.functionsCount);
         for(var i = 0; i < serviceInfo.functionsCount; i = i + 1) {
-            functionsTable[i] = cursor;
+            val rawFunction: RawFunctionInfo = cursor;
             cursor = cursor + functionInfoSize;
+            val function = alloc(runtimeFunctionSize);
+            functionsTable[i] = function;
+            function.address = rawFunction.address;
+            function.argsMemorySize = computeArgsMemorySize(rawFunction);
         }
 
         val result: ProgramInfo = alloc(sizeof<ProgramInfo>);
-        result.serviceInfo = serviceInfo;
+        result.mainIndex = serviceInfo.mainIndex;
         result.functionsTable = functionsTable;
+        result.functionsTableSize = serviceInfo.functionsCount;
         result.bytecode = rawBytecode + (rawBytecodeSize - bytecodeSize);
         result.bytecodeSize = bytecodeSize;
 
         return result;
      }
+
+    fn computeArgsMemorySize(raw: RawFunctionInfo): int {
+        var size = 0;
+        val count: byte = raw.argsCount;
+        val description: [byte] = cast<[void]>(raw) + sizeof<int>*2;
+        for (var i: byte = 0; i < count; i = i + 1) {
+            val index = i / 4;
+            val offset = i % 4;
+            val b = description[index];
+            val s = b >> offset;
+            if (s == 1 ) {
+                size = size + 1;
+            } else {
+                size = size + 4;
+            }
+        }
+
+        return size;
+    }
+
 """
