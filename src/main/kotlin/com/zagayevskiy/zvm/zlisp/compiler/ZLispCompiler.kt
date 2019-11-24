afe5d5a456debc397ad169a982db0b594aa7a890
Denis Zagayevskiy <zagayevskiy@gmail.com>
2019-11-24 03:11:46 +0300

    some code for compiling lisp

---
+++ src/main/kotlin/com/zagayevskiy/zvm/zlisp/compiler/ZLispCompiler.kt
@@ -0,0 +1,69 @@
+package com.zagayevskiy.zvm.zlisp.compiler
+
+import com.zagayevskiy.zvm.zc.ast.*
+import com.zagayevskiy.zvm.zc.types.UnresolvedType
+import com.zagayevskiy.zvm.zlisp.Sexpr
+
+class ZLispCompiler() {
+
+    private val atomVals = mutableMapOf<String, AstValDecl>()
+    private val numberVals = mutableMapOf<Int, AstValDecl>()
+    private val strVals = mutableMapOf<String, AstValDecl>()
+    private var nextId: Int = 0
+        get() = field++
+
+    fun compile() {
+        val program = listOf<Sexpr>()
+
+        val lispDecls = program.map { expr ->
+            AstExpressionStatement(expr.toStatement())
+        }
+
+        AstFunctionDeclaration(
+                name = "main",
+                returnType = null,
+                body = AstBlock(statements = atomVals.values + numberVals.values + strVals.values + lispDecls),
+                args = emptyList())
+    }
+
+    private val memory = AstIdentifier("memory")
+    private val cons = AstIdentifier("cons")
+    private val createAtom = AstIdentifier("createAtom")
+    private val createNumber = AstIdentifier("createNumber")
+    private val createString = AstIdentifier("createString")
+
+    private fun Sexpr.toStatement(): AstExpr {
+        //TODO includes
+        return when (this) {
+            is Sexpr.DotPair -> AstFunctionCall(cons, listOf(memory, head.toStatement(), tail.toStatement()))
+            is Sexpr.Atom -> obtainAtom(name)
+            is Sexpr.Number -> obtainNumber(value)
+            is Sexpr.Str -> obtainString(value)
+            Sexpr.Nil -> AstIdentifier("nil")
+            is Sexpr.RuntimeOnly -> throw IllegalStateException("Must not be here $this")
+        }
+    }
+
+    private fun obtainAtom(name: String): AstIdentifier {
+        return obtain("atom", name, atomVals, createAtom, AstConst.StringLiteral(name))
+    }
+
+    private fun obtainNumber(value: Int): AstIdentifier {
+        return obtain("number", value, numberVals, createNumber, AstConst.Integer(value))
+    }
+
+    private fun obtainString(value: String): AstIdentifier {
+        return obtain("str", value, strVals, createString, AstConst.StringLiteral(value))
+    }
+
+    private fun <T> obtain(valNamePrefix: String, value: T, cache: MutableMap<T, AstValDecl>, function: AstIdentifier, valExpr: AstExpr): AstIdentifier {
+        cache[value]?.let { valDecl ->
+            return AstIdentifier(valDecl.valName)
+        }
+
+        val valName = "${valNamePrefix}_$nextId"
+        val valDecl = AstValDecl(valName, null, AstFunctionCall(function, listOf(memory, valExpr)))
+        cache[value] = valDecl
+        return AstIdentifier(valName)
+    }
+}
