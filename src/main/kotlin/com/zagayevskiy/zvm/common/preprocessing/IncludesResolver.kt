2cbc04a2e47623b276aaa3c2a701d68707b180a9
Denis Zagayevskiy <zagayevskiy@gmail.com>
2020-09-30 23:30:38 +0300

    some improvements to includes resolving

--- src/main/kotlin/com/zagayevskiy/zvm/common/preprocessing/IncludesResolver.kt
+++ src/main/kotlin/com/zagayevskiy/zvm/common/preprocessing/IncludesResolver.kt
@@ -5,16 +5,26 @@ interface IncludesResolver {
 }
 
 abstract class AbsIncludesResolver : IncludesResolver {
-    private val alreadyIncluded = setOf<String>()
+    private val alreadyIncluded = mutableSetOf<String>()
 
     final override fun resolve(path: String): Sequence<Char> {
         if (alreadyIncluded.contains(path)) return emptySequence()
-        return doResolve(path)
+        return doResolve(path).also {
+            alreadyIncluded.add(path)
+        }
     }
 
     protected abstract fun doResolve(path: String): Sequence<Char>
 }
 
 class CompositeIncludesResolver(private val resolvers: List<IncludesResolver>): AbsIncludesResolver() {
-    override fun doResolve(path: String) = resolvers.asSequence().flatMap { it.resolve(path) }
+    override fun doResolve(path: String): Sequence<Char> {
+        resolvers.forEach {
+            val seq = it.resolve(path)
+            val iter = seq.iterator()
+            if (iter.hasNext()) return iter.asSequence()
+        }
+
+        throw IllegalStateException("can not resolve $path")
+    }
 }
